e87b8287a2408a4d6d81c044483eb9285142584d
Suysker <61983976+Suysker@users.noreply.github.com>
2023-04-21 23:41:29 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1772,14 +1772,19 @@ EOF
 
 # 定时任务更新tls证书
 installCronTLS() {
-	echoContent skyBlue "\n进度 $1/${totalProgress} : 添加定时维护证书"
-	crontab -l >/etc/xray-agent/backup_crontab.cron
-	local historyCrontab
-	historyCrontab=$(sed '/install.sh/d;/acme.sh/d' /etc/xray-agent/backup_crontab.cron)
-	echo "${historyCrontab}" >/etc/xray-agent/backup_crontab.cron
-	echo "30 1 * * * /bin/bash /etc/xray-agent/install.sh RenewTLS >> /etc/xray-agent/crontab_tls.log 2>&1" >>/etc/xray-agent/backup_crontab.cron
-	crontab /etc/xray-agent/backup_crontab.cron
-	echoContent green "\n ---> 添加定时维护证书成功"
+	if [[ -f "/etc/xray-agent/install.sh" ]]; then
+		echoContent skyBlue "\n进度 $1/${totalProgress} : 添加定时维护证书"
+		crontab -l >/etc/xray-agent/backup_crontab.cron
+		local historyCrontab
+		historyCrontab=$(sed '/install.sh/d;/acme.sh/d' /etc/xray-agent/backup_crontab.cron)
+		echo "${historyCrontab}" >/etc/xray-agent/backup_crontab.cron
+		echo "30 1 * * * /bin/bash /etc/xray-agent/install.sh RenewTLS >> /etc/xray-agent/crontab_tls.log 2>&1" >>/etc/xray-agent/backup_crontab.cron
+		crontab /etc/xray-agent/backup_crontab.cron
+		echoContent green "\n ---> 添加定时维护证书成功"
+	else
+		#删除自动更新证书
+		crontab -l | grep -v 'install.sh RenewTLS' | crontab -
+	fi
 }
 
 # 修改nginx重定向配置
@@ -1860,21 +1865,26 @@ handleNginx start
 
 # 更新geoip和geosite
 auto_update_geodata() {
-	cat > /etc/xray-agent/auto_update_geodata.sh << EOF
+	if [[ -f "/etc/xray-agent/xray/xray" ]] || [[ -f "/etc/xray-agent/xray/geosite.dat" ]] || [[ -f "/etc/xray-agent/xray/geoip.dat" ]]; then
+		cat > /etc/xray-agent/auto_update_geodata.sh << EOF
 #!/bin/sh
 wget -O /etc/xray-agent/xray/geosite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat && wget -O /etc/xray-agent/xray/geoip.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat && systemctl restart xray
 EOF
 
-	chmod +x /etc/xray-agent/auto_update_geodata.sh
+		chmod +x /etc/xray-agent/auto_update_geodata.sh
 
-	echoContent skyBlue "添加定时更新GeoData"
-	crontab -l >/etc/xray-agent/backup_crontab.cron
-	local historyCrontab
-	historyCrontab=$(sed '/auto_update_geodata.sh/d' /etc/xray-agent/backup_crontab.cron)
-	echo "${historyCrontab}" >/etc/xray-agent/backup_crontab.cron
-	echo "30 1 * * 1 /bin/bash /etc/xray-agent/auto_update_geodata.sh >> /etc/xray-agent/crontab_geo.log 2>&1" >>/etc/xray-agent/backup_crontab.cron
-	crontab /etc/xray-agent/backup_crontab.cron
-	echoContent green "\n ---> 添加定时更新GeoData成功"
+		echoContent skyBlue "添加定时更新GeoData"
+		crontab -l >/etc/xray-agent/backup_crontab.cron
+		local historyCrontab
+		historyCrontab=$(sed '/auto_update_geodata.sh/d' /etc/xray-agent/backup_crontab.cron)
+		echo "${historyCrontab}" >/etc/xray-agent/backup_crontab.cron
+		echo "30 1 * * 1 /bin/bash /etc/xray-agent/auto_update_geodata.sh >> /etc/xray-agent/crontab_geo.log 2>&1" >>/etc/xray-agent/backup_crontab.cron
+		crontab /etc/xray-agent/backup_crontab.cron
+		echoContent green "\n ---> 添加定时更新GeoData成功"
+	else
+		#删除更新geoip和geosite
+		crontab -l | grep -v 'auto_update_geodata.sh' | crontab -
+	fi
 
 }
 
